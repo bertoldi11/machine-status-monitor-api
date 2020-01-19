53298cafa8e0c271f3f63306858b7f8ba192ee68
Vinicius Bertoldi <bertoldi11@gmail.com>
2020-01-19 04:38:46 +0000

    Adding consumer_url to enable cors request

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express")
 const routes = require('./src/routes')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const consumerUrl = process.env.CONSUMER_URL || 'http://192.168.0.8:8080'
 
-app.use(cors({ origin: 'http://192.168.0.8:8080'}))
+app.use(cors({ origin: consumerUrl }))
 app.use(express.json())
 app.use('/', routes);
 app.listen(port, () => console.log(`App listening on port ${port}!`))
