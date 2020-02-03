492cb58ddc4ea81c440bf9b765e06adbee166658
Vinicius Bertoldi <bertoldi11@gmail.com>
2020-02-03 20:18:22 +0000

    Stoping cron job

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express")
 const cors = require('cors')
 const cron = require('node-cron')
 const routes = require('./src/routes')
-const { generateRandomStatus } = require("./scripts/generateRandomStatus")
 const app = express()
 const port = process.env.PORT || 3000
 const consumerUrl = process.env.CONSUMER_URL || 'http://192.168.0.8:8080'
 
-cron.schedule('*/30 * * * *', () => {
-    generateRandomStatus()
-});
+//cron.schedule('*/30 * * * *', () => {
+//    generateRandomStatus()
+//});
 
 app.use(cors({ origin: consumerUrl }))
 app.use(express.json())
